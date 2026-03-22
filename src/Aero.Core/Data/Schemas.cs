3c7b4e5e4d81ab288e2d451cec252846a771835f
bbqchickenrobot <bbqchickenrobot@gmail.com>
2026-03-21 19:31:51 -0500

    quick schema fix

--- src/Aero.Core/Data/Schemas.cs
+++ src/Aero.Core/Data/Schemas.cs
@@ -2,7 +2,7 @@ namespace Aero.Core.Data;
 
 public class Schemas
 {
-    public const string Api = "Api";
-    public const string Auth = "Auth";
-    public const string Aero = "Aero";
+    public const string Api = "api";
+    public const string Auth = "auth";
+    public const string Aero = "aero";
 }
