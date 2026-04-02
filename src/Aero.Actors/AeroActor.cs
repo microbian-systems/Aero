9c2b3b537294aee75e467f65c2bae3bdcf9eaea8
bbqchickenrobot <bbqchickenrobot@gmail.com>
2026-04-02 15:14:01 -0500

    misc orleans fixes

--- src/Aero.Actors/AeroActor.cs
+++ src/Aero.Actors/AeroActor.cs
@@ -1,23 +1,37 @@
+using OpenTelemetry.Resources;
+using System.Diagnostics;
+
 namespace Aero.Actors;
+
+
+public interface IAeroActor : IGrainWithIntegerKey;
 
 /// <summary>
 /// Base grain class for actors
 /// </summary>
 /// <param name="log">ILogger<T/> instance for logging</param>
-public abstract class AeroGrain(ILogger<AeroGrain> log) : Grain
+public abstract class AeroActor(ILogger<AeroActor> log) : Grain, IAeroActor
 {
+    protected static readonly ActivitySource Span = new(nameof(AeroActor));
+
     public override async Task OnActivateAsync(CancellationToken cancellationToken)
     {
+        using var activity = Span.StartActivity($"{GetType().Name}.Activated");
+        activity?.SetTag("grain.id", this.GetGrainId().ToString());
+
         await base.OnActivateAsync(cancellationToken);
+
+        log.LogInformation("Actor {Type} activated", GetType().Name);
     }
 
     public override async Task OnDeactivateAsync(DeactivationReason reason, CancellationToken cancellationToken)
     {
         if (reason.ReasonCode == DeactivationReasonCode.ShuttingDown)
         {
-            log.LogInformation("Grain deactivated ... shutting down");
+            log.LogInformation($"{this.GetType().Name} - {this.GetGrainId()} - actor deactivated");
             MigrateOnIdle();
         }
         await base.OnDeactivateAsync(reason, cancellationToken);
     }
 }
+
