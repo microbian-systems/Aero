e569c2024382a83123c631d215efe0f7a79694de
bbqchickenrobot <bbqchickenrobot@gmail.com>
2026-05-01 18:24:46 -0500

    misc

--- src/Aero.Web/Extensions/AppSettingsExtensions.cs
+++ src/Aero.Web/Extensions/AppSettingsExtensions.cs
@@ -4,14 +4,21 @@ namespace Aero.Web.Extensions;
 
 public static class AppSettingsExtensions
 {
-    public static WebApplicationBuilder AddAppSettings(this WebApplicationBuilder builder)
+    public static WebApplicationBuilder AddAppSettings<T>(this WebApplicationBuilder builder)
+        where T : class
     {
         var env = builder.Environment;
+
         builder.Configuration
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            .AddEnvironmentVariables()
-            .AddUserSecrets(Assembly.GetExecutingAssembly());
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        if (env.IsDevelopment())
+        {
+            // allegedly aot compatible
+            builder.Configuration.AddUserSecrets<T>(optional: true);
+        }
 
         return builder;
     }
